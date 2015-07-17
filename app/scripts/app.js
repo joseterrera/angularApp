bb381cb229b7c37f09933d8133c327898499db23
joseterrera <joseterrera@yahoo.com>
2015-07-17 11:05:06 -0500

    deleted some files added posts.html and posts.js

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+/* global app:true  */
+/* exported app */
+
+
+'use strict';
+var app = angular
+  .module('angularAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
